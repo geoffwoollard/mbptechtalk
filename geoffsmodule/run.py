2dfc594d1702a91d2f1fa4a3021eda141c70849d
Geoffrey Woollard <geoffwoollard@gmail.com>
2019-01-18 12:53:44 -0500

    relative path for main.log

--- geoffsmodule/run.py
+++ geoffsmodule/run.py
@@ -1,9 +1,9 @@
-import gpu, mymath
+from . import gpu, mymath
 import logging
 import numpy as np
 
 def main():
-    logging.basicConfig(filename='/Users/gw/Dropbox/2018w/mbptechtalk/main.log', level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.basicConfig(filename='main.log', level=logging.INFO, format='%(asctime)s %(message)s')
     logging.info('Started')
     a = np.ones((5,100000))
     b = np.ones((100000,3))
