dbadcc0d955e31a6226b9f7697812b2ea4fd5ec3
Geoffrey Woollard <geoffwoollard@gmail.com>
2019-01-18 17:24:02 -0500

    doc string

--- geoffsmodule/gpu.py
+++ geoffsmodule/gpu.py
@@ -4,7 +4,11 @@ import numpy as np
 
 @jit
 def mm_jit(a,b):
-    '''matrix multiply'''
+    '''matrix multiply (ixj)x(kxl)
+    param a: ndarray,  the input array (ixj)
+    param b: ndarray, the other input array (kxl)
+    return c: ndarray, the output array (ixl)
+    '''
     I,J = a.shape
     K,L = b.shape
     assert J == K, 'cols of a must match rows of b for matrix multiplication'
