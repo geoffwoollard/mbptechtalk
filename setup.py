6a6af184596c976cb9fbed06e8e8b26d87c185af
Geoffrey Woollard <geoffwoollard@gmail.com>
2019-01-18 12:55:15 -0500

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'geoffsmodule',         # How you named your package folder (MyLib)
   packages = ['geoffsmodule'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Materials for MBP Tech Talk on Software Engineering',   # Give a short description about your library
   author = 'Geoffrey Woollard',                   # Type in your name
